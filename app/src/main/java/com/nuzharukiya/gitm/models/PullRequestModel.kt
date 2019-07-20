7d570abeffbc98fe1cd8cbc1ebbe59f79059100a
Nuzha Rukiya <r.nuzha@gmail.com>
2019-07-20 15:42:58 +0530

    Created model for response

--- app/src/main/java/com/nuzharukiya/gitm/models/PullRequestModel.kt
+++ app/src/main/java/com/nuzharukiya/gitm/models/PullRequestModel.kt
@@ -4,7 +4,13 @@ package com.nuzharukiya.gitm.models
  * Created by Nuzha Rukiya on 19/07/19.
  */
 
-// Dummy
 data class PullRequestModel(
-        val prName: String
+        val title: String,
+        val number: Int,
+        val user: UserModel,
+        val created_at: String
+)
+
+data class UserModel(
+        val login: String
 )
